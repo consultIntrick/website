bb9c3cda2b58f0bdc3ede2cc1c41acece438abff
Kukhapprabu-Pydan <kprabu@pydan.com>
2016-12-10 18:35:08 +0530

    Debug made true

--- intrick/settings.py
+++ intrick/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1*2bubrbamjzj-(60#bxfg7p_9(n%o9vrf=ym-fjes$v=k7p!8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
